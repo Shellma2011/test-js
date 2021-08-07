44b2671a24ebb703ea24d7d3bc8436d7f5058be5
Maryna Shevchenko <Shellma2011@gmail.com>
2021-08-07 18:53:49 +0300

    07.08.2021

---
+++ js/script-task-6.js
@@ -0,0 +1,15 @@
+  
+// Цикл white или do...while
+// Запрашивать число пока не будет больше 100
+
+let result = 0;
+
+// do {
+//   result = prompt('Enter number under 100');
+// } while (result < 100);
+
+while (result < 100) {
+  result = prompt('Enter number under 100');
+}
+
+console.log(result);
