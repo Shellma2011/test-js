8d682ddcb5df72f3e85009a4125153192123e2b9
Maryna Shevchenko <Shellma2011@gmail.com>
2021-08-20 00:27:31 +0300

    20.08.21

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 // import "./js/to-string.js"
 // import "./js/to-number.js"
-import "./js/to-boolean.js"
+// import "./js/to-boolean.js"
+import "./js/test.js"
---
+++ js/test.js
@@ -0,0 +1,111 @@
+// let userNumber = prompt('set number');
+// let userPow = prompt('set pow');
+// const number = Number(userNumber);
+// const pow = Number(userPow); 
+// console.log(typeof number);
+
+
+// console.log(Math.pow(pow, number));
+////////////////////////////////////////////////////////////////////////////////////////
+// let num = '12345';
+// let total = 1;
+
+// for (let i = 1; i < num.length; i += 1) {
+//     total *= num[i]
+
+// }
+
+// console.log(total)
+
+////////////////////////////////////////////////////////////
+
+// let hours = prompt('Введите часы');
+// let days = prompt('Введите дни');
+// let mounth = prompt('Введите месяцы');
+
+// const userHours = hours * (60 * 60);
+// const userDay = days * (60 * 60 * 24);
+// const userMounth = mounth * (60 * 60 * 24 * 30);
+
+// console.log(`количество секунд в часе: ${userHours}`);
+// console.log(`количество секунд в дне: ${userDay}`);
+// console.log(`количество секунд в месяце: ${userMounth}`);
+
+// /////////////////////////////////////////////////////
+
+// let hours = prompt('Введите часы');
+// let minut = prompt('Введите минуты');
+// let seconde = prompt('Введите секунды');
+
+// console.log(`${hours}:${minut}:${seconde}`)
+
+//////////////////////////////////////////////////////
+
+// if (1 === '1') {
+//     console.log('истина');
+// } else {
+//     console.log('ложь');
+// }
+
+// if (5 === '5') {
+//     console.log('истина');
+// } else {
+//     console.log('ложь');
+// }
+
+// let message = (92 > '11' && 58 < 100)
+//     ?
+//     'истина' : 'ложь';
+// console.log(message);
+    
+////////////////////////////////////////////////////
+
+// let a = "" - 1 + 0;
+// console.log(a);
+
+/////////////////////////////////////////////////////
+
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// a = Number(a);
+// b = Number(b)
+
+// alert(a + b); // 12
+
+/////////////////////////////////////////////////////
+
+if ("0") {
+  alert( 'Привет' );
+}
+
+/////////////////////////////////////////////////////
+
+// let value = prompt('Какое "официальное" название JavaScript?', '');
+
+// if (value === 'ECMAScript') {
+//     alert('Правильно!');
+// } else {
+//     'Не знаете? “ECMAScript”!'
+// }
+
+//////////////////////////////////////////////////////
+
+// let value = prompt("введите число")
+
+// if (value > 0) {
+//     alert("1");
+// } else if (value < 0) {
+//     alert("-1");
+// } else {
+// alert("0");
+// }
+
+///////////////////////////////////////////////////
+
+// let result;
+
+// result = (a + b < 4) ? 'Мало' : 'Много';
+
+// console.log(result)
+
